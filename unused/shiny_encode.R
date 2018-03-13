aecb0806f765c7efceeaf9234c1725e54129b00d
andronekomimi <lemacon.audrey@gmail.com>
2018-03-13 16:23:58 -0400

    moving unused files and dir

--- unused/shiny_encode.R
+++ unused/shiny_encode.R
@@ -1,5 +1,7 @@
+library(shiny)
+
 shinyEncode <- function(){
   source(file = system.file("inst/shiny/ui.R", package = "ENCODExplorer"))
   source(file = system.file("shiny/server.R", package = "ENCODExplorer"))
-    runApp(appDir = shinyApp(ui,server))
+    runApp(appDir = shiny::shinyApp(ui,server))
 }
